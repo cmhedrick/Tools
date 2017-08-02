5c2e18ee57eafa1b5f184b6a4b23123be6a693fd
cmhedrick <christopher.m.hedrick@gmail.com>
2017-08-02 19:11:54 -0400

    DAY 54/100 a peek at my bashrc :P

---
+++ bashrc.sh
@@ -0,0 +1,16 @@
+# yes I use VIM
+export EDITOR=vim
+# update everything quickly
+alias quickUpdate="sudo apt-get update && sudo apt-get dist-upgrade -y"
+# turn off IPv6 for crappy VPN
+alias ipv6Kill="sudo sh -c 'echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6'"
+alias ipv6Restore="sudo sh -c 'echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6'"
+# working with git
+alias s='git status'
+alias d='git diff HEAD | colordiff | less -R'
+# django development
+alias act='source env/bin/activate'
+alias rs='python manage.py runserver 0.0.0.0:8000'
+# init raspberry pi connection
+alias raspi='sudo screen /dev/ttyUSB0 115200'
+
