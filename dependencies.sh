6ce2e49836861f2c8894ca63f5473e58e87195ef
cmhedrick <christopher.m.hedrick@gmail.com>
2018-01-19 13:08:23 -0500

    added shutter for screenshots

--- dependencies.sh
+++ dependencies.sh
@@ -9,4 +9,4 @@
 # (assists in building packages), Virtualbox Extentions (extends vbox addons),
 # Libraries to support pillow and image types.
 
-sudo apt-get install synaptic git netcat aircrack-ng nmap vim colordiff virtualenvwrapper virtualbox virtualbox-ext-pack virtualbox-guest-dkms virtualbox-guest-additions-iso python2.7-dev python3-dev python-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev tcl8.6-dev tk8.6-dev python-tk gimp curl gparted hexchat inkscape pwgen wireshark macchanger
+sudo apt-get install synaptic git netcat aircrack-ng nmap vim colordiff virtualenvwrapper virtualbox virtualbox-ext-pack virtualbox-guest-dkms virtualbox-guest-additions-iso python2.7-dev python3-dev python-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev tcl8.6-dev tk8.6-dev python-tk gimp curl gparted hexchat inkscape pwgen wireshark macchanger shutter
