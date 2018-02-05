96a0dcccfdec369b288b8310d235094dbc0ab80e
cmhedrick <christopher.m.hedrick@gmail.com>
2018-02-04 21:28:29 -0500

    added secure-delete

--- dependencies.sh
+++ dependencies.sh
@@ -8,5 +8,6 @@
 # Colordiff (colorizes git changes using git status), Python Dev packages 
 # (assists in building packages), Virtualbox Extentions (extends vbox addons),
 # Libraries to support pillow and image types.
+# secure-delete for all your secure deleting needs
 
-sudo apt-get install synaptic git netcat aircrack-ng nmap vim colordiff virtualenvwrapper virtualbox virtualbox-ext-pack virtualbox-guest-dkms virtualbox-guest-additions-iso python2.7-dev python3-dev python-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev tcl8.6-dev tk8.6-dev python-tk gimp curl gparted hexchat inkscape pwgen wireshark macchanger shutter
+sudo apt-get install synaptic git netcat aircrack-ng nmap vim colordiff virtualenvwrapper virtualbox virtualbox-ext-pack virtualbox-guest-dkms virtualbox-guest-additions-iso python2.7-dev python3-dev python-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev tcl8.6-dev tk8.6-dev python-tk gimp curl gparted hexchat inkscape pwgen wireshark macchanger shutter secure-delete
