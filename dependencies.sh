6f18b0c7f484a457ae64d706a77f7625baa5a254
cmhedrick <christopher.m.hedrick@gmail.com>
2018-02-26 18:49:45 -0500

    removed virtualbox. INSTALL VIRTUAL BOX FROM SITE

--- dependencies.sh
+++ dependencies.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 # Core Installs:
 # Synaptic Package Manager, Git, NetCat, Aircrack-ng, VIM, Virtualenv, 
-# Virtual Box, CURL, GIMP, G-PARTED, HEXChat, Inkscape, pwgen, WireShark
+# CURL, GIMP, G-PARTED, HEXChat, Inkscape, pwgen, WireShark
 # nmap, MACChanger
 
 # Plugin Installs:
 # Colordiff (colorizes git changes using git status), Python Dev packages 
-# (assists in building packages), Virtualbox Extentions (extends vbox addons),
+# (assists in building packages),
 # Libraries to support pillow and image types.
 # secure-delete for all your secure deleting needs
 
-sudo apt-get install synaptic git netcat aircrack-ng nmap vim colordiff virtualenvwrapper virtualbox virtualbox-ext-pack virtualbox-guest-dkms virtualbox-guest-additions-iso python2.7-dev python3-dev python-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev tcl8.6-dev tk8.6-dev python-tk gimp curl gparted hexchat inkscape pwgen wireshark macchanger shutter secure-delete
+sudo apt-get install synaptic git netcat aircrack-ng nmap vim colordiff virtualenvwrapper python2.7-dev python3-dev python-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev tcl8.6-dev tk8.6-dev python-tk gimp curl gparted hexchat inkscape pwgen wireshark macchanger shutter secure-delete
