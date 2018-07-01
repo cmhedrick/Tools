ee2756023ec49081a39500d973caa007a1bb36d9
cmhedrick <christopher.m.hedrick@gmail.com>
2018-07-01 17:42:37 -0400

    added openvpn for ubuntu18.04

--- dependencies.sh
+++ dependencies.sh
@@ -10,4 +10,4 @@
 # Libraries to support pillow and image types.
 # secure-delete for all your secure deleting needs
 
-sudo apt-get install synaptic git netcat aircrack-ng nmap vim colordiff virtualenvwrapper python2.7-dev python3-dev python-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev tcl8.6-dev tk8.6-dev python-tk gimp curl gparted hexchat inkscape pwgen wireshark macchanger shutter secure-delete
+sudo apt-get install synaptic git netcat aircrack-ng nmap vim colordiff virtualenvwrapper python2.7-dev python3-dev python-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev tcl8.6-dev tk8.6-dev python-tk gimp curl gparted hexchat inkscape pwgen wireshark macchanger shutter secure-delete openvpn network-manager-openvpn network-manager-openvpn-gnome
