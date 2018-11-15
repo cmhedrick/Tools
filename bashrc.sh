64a6d35ae3261d8c2ec8135a59ba7def4173defb
cmhedrick <christopher.m.hedrick@gmail.com>
2018-11-15 16:11:36 -0500

    change apt-get to apt only - NaSWriMo 06

--- bashrc.sh
+++ bashrc.sh
@@ -1,7 +1,7 @@
 # yes I use VIM
 export EDITOR=vim
 # update everything quickly
-alias quickUpdate="sudo apt-get update && sudo apt-get dist-upgrade -y"
+alias quickUpdate="sudo apt update && sudo apt dist-upgrade -y"
 # turn off IPv6 for crappy VPN
 alias ipv6Kill="sudo sh -c 'echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6'"
 alias ipv6Restore="sudo sh -c 'echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6'"
