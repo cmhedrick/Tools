60d11cdfe48cce63121a5e8c6580def5028dd826
cmhedrick <christopher.m.hedrick@gmail.com>
2019-05-08 22:39:04 -0400

    have cron script restart process

--- cronjob-example.sh
+++ cronjob-example.sh
@@ -11,8 +11,13 @@ PIDFILE="$HOME/tmp/program.pid"
 # if running exit script
 if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
                            grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
-  echo "[-] Currently running"
-  exit 99
+  echo "[-] Restarting"
+  # read in PID File
+  while IFS= read -r var
+  do
+    # SIGKILL process
+    kill -9 $var
+  done < "$PIDFILE"
 fi
 
 path/to/program-to-cron.sh > $HOME/path/to/program.log &
